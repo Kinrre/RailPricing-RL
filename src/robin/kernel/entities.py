fc031c3b7abf7bf0ce44b17eea4692e107523556
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2024-03-10 00:18:48 +0100

    feat: add simulate_a_day in kernel

--- src/robin/kernel/entities.py
+++ src/robin/kernel/entities.py
@@ -1,5 +1,7 @@
 """Entities for the kernel module."""
 
+import datetime
+import logging
 import numpy as np
 import pandas as pd
 import random
@@ -9,7 +11,7 @@ from src.robin.demand.entities import Demand, Passenger
 from src.robin.supply.entities import Service, Supply
 
 from pathlib import Path
-from typing import List, Union
+from typing import Dict, List, Union
 
 
 class Kernel:
@@ -19,6 +21,10 @@ class Kernel:
     Attributes:
         supply (Supply): Supply object.
         demand (Demand): Demand object.
+        passengers (List[Passenger]): List of passengers.
+        passengers_purchase_day (Dict[datetime.date, List[Passenger]]): Dictionary with passengers grouped by purchase day.
+        simulation_days (List[datetime.date]): List of simulation days.
+        last_simulated_day_idx (Union[int, None]): Index of the last simulated day.
     """
     
     def __init__(self, path_config_supply: Path, path_config_demand: Path, seed: Union[int, None] = None) -> None:
@@ -34,68 +40,61 @@ class Kernel:
             self.set_seed(seed)
         self.supply = Supply.from_yaml(path_config_supply)
         self.demand = Demand.from_yaml(path_config_demand)
+        self.passengers = self.demand.generate_passengers()
+        self.passengers_purchase_day = self._group_passengers_by_purchase_day(self.passengers)
+        self.simulation_days = self._get_simulation_days()
+        self.last_simulated_day_idx = None
 
-    def _to_csv(self, passengers: List[Passenger], output_path: Path = Path('output.csv')) -> None:
+    def _get_simulation_days(self) -> List[datetime.date]:
         """
-        Save passengers data to csv file.
+        Get the simulation days (purchase days of the passengers).
+        
+        Returns:
+            List[datetime.date]: List of simulation days.
+        """
+        simulation_days = list(self.passengers_purchase_day.keys())
+        simulation_days.sort()
+        return simulation_days
 
+    def _group_passengers_by_purchase_day(self, passengers: List[Passenger]) -> Dict[datetime.date, List[Passenger]]:
+        """
+        Group passengers by purchase day.
+        
         Args:
             passengers (List[Passenger]): List of passengers.
-            output_path (Path, optional): Path to the output csv file. Defaults to 'output.csv'.
+        
+        Returns:
+            List[List[Passenger]]: List of passengers grouped by purchase day.
         """
-        column_names = [
-            'id', 'user_pattern', 'departure_station', 'arrival_station',
-            'arrival_day', 'arrival_time', 'purchase_day', 'service', 'service_departure_time',
-            'service_arrival_time', 'seat', 'price', 'utility', 'best_service', 'best_seat', 'best_utility'
-        ]
-        data = []
+        passengers_purchase_day = {}
         for passenger in passengers:
-            data.append([
-                passenger.id,
-                passenger.user_pattern,
-                passenger.market.departure_station,
-                passenger.market.arrival_station,
-                passenger.arrival_day,
-                passenger.arrival_time,
-                passenger.purchase_day,
-                passenger.service,
-                passenger.service_departure_time,
-                passenger.service_arrival_time,
-                passenger.seat,
-                passenger.ticket_price,
-                passenger.utility,
-                passenger.best_service,
-                passenger.best_seat,
-                passenger.best_utility
-            ])
-        df = pd.DataFrame(data=data, columns=column_names)
-        df.to_csv(output_path, index=False)
+            purchase_day = passenger.purchase_day
+            if purchase_day in passengers_purchase_day:
+                # Append the passenger to the list
+                passengers_purchase_day[purchase_day].append(passenger)
+            else:
+                # Create a new list with the passenger
+                passengers_purchase_day[purchase_day] = [passenger]
+        return passengers_purchase_day
 
-    def simulate(
+    def _simulate(
             self,
+            passengers: List[Passenger],
             output_path: Union[Path, None] = None,
             departure_time_hard_restriction: bool = True,
             calculate_global_utility: bool = False
-        ) -> List[Service]:
+        ) -> None:
         """
-        Simulate the demand-supply interaction.
-
-        The passengers will maximize the utility for each service and seat, according to
-        its origin-destination and date, buying a ticket only if the utility is positive.
-
+        Private method to simulate the demand-supply interaction.
+        
         Args:
+            passengers (List[Passenger]): List of passengers.
             output_path (Path, optional): Path to the output csv file. Defaults to None.
             departure_time_hard_restriction (bool, optional): If True, the passenger will not
                 be assigned to a service with a departure time that is not valid. Defaults to True.
             calculate_global_utility (bool, optional): If True, it will be calculated the global utility
                 for each seat, even if no tickets are available. Defaults to False.
-
-        Returns:
-            List[Service]: List of services with updated tickets.
         """
-        # Generate passengers demand
-        passengers = self.demand.generate_passengers()
-        
         for passenger in passengers:
             # Filter services by passenger's origin-destination and date
             origin = passenger.market.departure_station
@@ -179,8 +178,113 @@ class Kernel:
         if output_path is not None:
             self._to_csv(passengers, output_path)
 
+    def _to_csv(self, passengers: List[Passenger], output_path: Path = Path('output.csv')) -> None:
+        """
+        Save passengers data to CSV file.
+
+        Args:
+            passengers (List[Passenger]): List of passengers.
+            output_path (Path, optional): Path to the output CSV file. Defaults to 'output.csv'.
+        """
+        column_names = [
+            'id', 'user_pattern', 'departure_station', 'arrival_station',
+            'arrival_day', 'arrival_time', 'purchase_day', 'service', 'service_departure_time',
+            'service_arrival_time', 'seat', 'price', 'utility', 'best_service', 'best_seat', 'best_utility'
+        ]
+        data = []
+        for passenger in passengers:
+            data.append([
+                passenger.id,
+                passenger.user_pattern,
+                passenger.market.departure_station,
+                passenger.market.arrival_station,
+                passenger.arrival_day,
+                passenger.arrival_time,
+                passenger.purchase_day,
+                passenger.service,
+                passenger.service_departure_time,
+                passenger.service_arrival_time,
+                passenger.seat,
+                passenger.ticket_price,
+                passenger.utility,
+                passenger.best_service,
+                passenger.best_seat,
+                passenger.best_utility
+            ])
+        df = pd.DataFrame(data=data, columns=column_names)
+        df.to_csv(output_path, index=False)
+
+    def simulate(
+            self,
+            output_path: Union[Path, None] = None,
+            departure_time_hard_restriction: bool = True,
+            calculate_global_utility: bool = False
+        ) -> List[Service]:
+        """
+        Simulate the demand-supply interaction.
+
+        The passengers will maximize the utility for each service and seat, according to
+        its origin-destination and date, buying a ticket only if the utility is positive.
+
+        Args:
+            output_path (Path, optional): Path to the output csv file. Defaults to None.
+            departure_time_hard_restriction (bool, optional): If True, the passenger will not
+                be assigned to a service with a departure time that is not valid. Defaults to True.
+            calculate_global_utility (bool, optional): If True, it will be calculated the global utility
+                for each seat, even if no tickets are available. Defaults to False.
+
+        Returns:
+            List[Service]: List of services with updated tickets.
+        """
+        self._simulate(
+            passengers=self.passengers,
+            output_path=output_path,
+            departure_time_hard_restriction=departure_time_hard_restriction,
+            calculate_global_utility=calculate_global_utility
+        )
         return self.supply.services
-    
+  
+    def simulate_a_day(
+            self,
+            output_path: Union[Path, None] = None,
+            departure_time_hard_restriction: bool = True,
+            calculate_global_utility: bool = False
+        ) -> List[Service]:
+        """
+        Simulate the demand-supply interaction for a day.
+        
+        The difference with the simulate method is that this method will only simulate the first available purchase day
+        of the passengers. This method is useful to simulate the demand-supply interaction for a day, and then
+        modify the supply object and simulate again the next day, for example, for a RL environment.
+
+        Args:
+            output_path (Path, optional): Path to the output csv file. Defaults to None.
+            departure_time_hard_restriction (bool, optional): If True, the passenger will not
+                be assigned to a service with a departure time that is not valid. Defaults to True.
+            calculate_global_utility (bool, optional): If True, it will be calculated the global utility
+                for each seat, even if no tickets are available. Defaults to False.
+
+        Returns:
+            List[Service]: List of services with updated tickets.
+        """
+        # Update last simulated day index
+        if self.last_simulated_day_idx is None:
+            self.last_simulated_day_idx = 0
+        else:
+            self.last_simulated_day_idx += 1
+            if self.last_simulated_day_idx >= len(self.simulation_days):
+                logging.warn('All days have been simulated, simulation will not continue.')
+                return self.supply.services
+        
+        # Simulate demand-supply interaction for the next available purchase day
+        self._simulate(
+            passengers=self.passengers_purchase_day[self.simulation_days[self.last_simulated_day_idx]],
+            output_path=output_path,
+            departure_time_hard_restriction=departure_time_hard_restriction,
+            calculate_global_utility=calculate_global_utility
+        )
+        return self.supply.services
+
     def set_seed(self, seed: int) -> None:
         """
         Set seed for the random number generator.
