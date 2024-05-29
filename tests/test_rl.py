25ae1e15be31b77b228d8f2b1370d9aaa078ac68
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2024-05-29 15:12:46 +0200

    fix: update to use factory env

--- tests/test_rl.py
+++ tests/test_rl.py
@@ -1,11 +1,9 @@
 import numpy as np
 
-from src.robin.rl.entities import FlattenAction, FlattenObservation, RobinEnv
+from src.robin.rl.entities import RobinEnvFactory
 
-env = RobinEnv(path_config_demand='configs/rl/demand_data.yml', path_config_supply='configs/rl/supply_data.yml')
+env = RobinEnvFactory.create(path_config_demand='configs/rl/demand_data.yml', path_config_supply='configs/rl/supply_data.yml')
 print(f'Number of services: {len(env.kernel.supply.services)}')
-env = FlattenObservation(env)
-env = FlattenAction(env)
 print(env.observation_space)
 print(env.action_space)
 
--- tests/test_train_sb3.py
+++ tests/test_train_sb3.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from src.robin.rl.entities import FlattenAction, FlattenObservation, RobinEnv
+from src.robin.rl.entities import RobinEnvFactory
 
 from stable_baselines3 import SAC, TD3
 from stable_baselines3.common.env_util import make_vec_env, SubprocVecEnv
@@ -9,20 +9,16 @@ from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3.common.vec_env import VecNormalize
 
 
-def make_env():
-    # Experiments have been conducted with departure time hard restriction set to false
-    env = RobinEnv(path_config_demand='configs/rl/demand_data.yml', path_config_supply='configs/rl/supply_data.yml')
-    env = FlattenObservation(env)
-    env = FlattenAction(env)
-    return env
-
-
 if __name__ == '__main__':
     # Train then model
     num_cpu = 16
-    log_name = 'TD3'
+    log_name = 'delete'
     log_dir = f'models/{log_name}'
-    vec_env = make_vec_env(make_env, n_envs=num_cpu, vec_env_cls=SubprocVecEnv)
+    vec_env = make_vec_env(
+        lambda: RobinEnvFactory.create(path_config_demand='configs/rl/demand_data.yml', path_config_supply='configs/rl/supply_data.yml'),
+        n_envs=num_cpu,
+        vec_env_cls=SubprocVecEnv
+    )
     vec_env = VecNormalize(vec_env, clip_obs=10.0, clip_reward=10.0)
     action_noise = NormalActionNoise(mean=np.zeros(10), sigma=0.1 * np.ones(10))
     model = TD3('MlpPolicy', vec_env, learning_starts=10_000, learning_rate=1e-4, action_noise=action_noise, train_freq=1, gradient_steps=-1, tensorboard_log='models', buffer_size=500_000, verbose=0)
@@ -32,7 +28,11 @@ if __name__ == '__main__':
 
     # Evaluate the model
     del vec_env, model
-    vec_env = make_vec_env(make_env, n_envs=num_cpu, vec_env_cls=SubprocVecEnv)
+    vec_env = make_vec_env(
+        lambda: RobinEnvFactory.create(path_config_demand='configs/rl/demand_data.yml', path_config_supply='configs/rl/supply_data.yml'),
+        n_envs=num_cpu,
+        vec_env_cls=SubprocVecEnv
+    )
     vec_env = VecNormalize.load(f'{log_dir}_1/vec_normalize.pkl', vec_env)
     vec_env.training = False
     vec_env.norm_reward = False
